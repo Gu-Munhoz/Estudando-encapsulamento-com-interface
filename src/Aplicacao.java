d985e7706b48db7f1c9f95c0e1bc41dca606bc9d
Gu-Munhoz <gumunhoz30@gmail.com>
2022-02-06 03:03:39 -0300

    Teste do controle

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,2 +1,12 @@
 public class Aplicacao {
+    public static void main(String[] args) {
+        Controle c = new Controle();
+        c.ligar();
+        c.maisVolume();
+        c.maisVolume();
+        c.pause();
+        c.play();
+        c.abrirMenu();
+
+    }
 }
